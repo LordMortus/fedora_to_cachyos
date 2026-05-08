556d4a82c1a801ab2e5f69cfa5ecbc3b4d41794b
LordMortus <lordmortus@gmail.com>
2026-05-08 12:16:52 -0500

    Update install.sh to include -y for dnf commands
    Added '-y' option to various dnf commands for automatic confirmation.

--- install.sh
+++ install.sh
@@ -7,31 +7,31 @@
 
 #Add video card in Proxmox after initial install first
 
-sudo dnf upgrade --refresh
-sudo dnf upgrade
+sudo dnf upgrade --refresh -y
+sudo dnf upgrade -y
 
 #RPM Fusion commands can be found searching "eneable rpm repositories"
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install copr-frontend-fedora.noarch
-sudo dnf instal copr
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install copr-frontend-fedora.noarch -y
+sudo dnf instal copr -y
 
 # sudo setsebool -P domain_kernel_load_modules on
 
-sudo dnf copr enable bieszczaders/kernel-cachyos
-sudo dnf copr enable bieszczaders/kernel-cachyos-addons
+sudo dnf copr enable bieszczaders/kernel-cachyos -y
+sudo dnf copr enable bieszczaders/kernel-cachyos-addons -y
 
-sudo dnf install kernel-cachyos kernel-cachyos-devel-matched
-sudo dnf in cachyos-settings scx-manager scx-scheds-git scx-tools-git --allowerasing
-sudo dnf upgrade --refresh
-sudo dnf upgrade
+sudo dnf install kernel-cachyos kernel-cachyos-devel-matched -y
+sudo dnf in cachyos-settings scx-manager scx-scheds-git scx-tools-git --allowerasing -y
+sudo dnf upgrade --refresh -y
+sudo dnf upgrade -y
 
 # dnf search kernel-cachyos
 # sudo dnf install <modules>
 
-sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg -y
 
-sudo dnf install kernel-devel kernel-headers gcc make dkms acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig libxcb egl-wayland
+sudo dnf install kernel-devel kernel-headers gcc make dkms acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig libxcb egl-wayland -y
 
 #Downlaod Nvidia drivers (.run)
 #Make executable
@@ -39,3 +39,4 @@ sudo dnf install kernel-devel kernel-headers gcc make dkms acpid libglvnd-glx li
 #Select NO to xconfig utility
 
 #REBOOT NOW
+reboot
