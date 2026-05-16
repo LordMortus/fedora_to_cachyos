c03286cd1a7513645bb26c9760d3331e562922f0
LordMortus <lordmortus@gmail.com>
2026-05-16 09:19:30 -0500

    Change script name in user switch command
    Reverted back to script1 as it is the cachyos script

--- bootstrap.sh
+++ bootstrap.sh
@@ -99,4 +99,4 @@ echo ""
 sudo bash -c "sleep 10 && userdel -r setup 2>/dev/null" &
 
 # === SWITCH TO NEW USER AND RUN SCRIPT 1 ===
-sudo su - "$NEW_USER" -c "cd ~/fedora_to_cachyos && bash script-niri.sh"
+sudo su - "$NEW_USER" -c "cd ~/fedora_to_cachyos && bash script1-base.sh"
